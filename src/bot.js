8db123c04e3de2d82a3b60cf7294847622f44794
insomniac94 <brandonhough42@gmail.com>
2022-02-17 20:20:16 -0500

    API setup

--- src/bot.js
+++ src/bot.js
@@ -1,20 +1,36 @@
-require("dotenv").config();
 
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+import dotenv from 'dotenv'
+import fetch from 'node-fetch'
+import Discord from 'discord.js'
+
+dotenv.config()
+
+const client = new Discord.Client({intents: ['GUILDS','GUILD_MESSAGES']})
 
 client.on('ready', () => {
-    console.log(`${client.user.tag} has logged in`)
-});
+    console.log(`Logged in as ${client.user.tag}`)
+})
 
-client.on('message', (message) => {
-    console.log(`${message.content}`);
-    if (message.content == "!start_stream") {
-        message.reply('Starting stream...');
-    }
-    if (message.content == "!end_stream") {
-        message.reply('Ending stream...');
+client.on('messageCreate', (message) => {
+    let channel = message.channel;
+
+    if(message.content == '.start_stream')
+    channel.createInvite({unique: true, maxAge: 21600})
+    .then(invite => {
+        const discord_link = 'https://discord.com/' + invite.code
+        const url = 'https://api.nightbot.tv/1/commands/5e9cb8960da0ca095b5cdf84'
+        const data = { message : 'Updated Link' + discord_link}
+        const headers = { Authorization: process.env.NIGHT_BOT_ACCESS_TOKEN}
+
+        fetch(url, { method: 'POST', headers: headers, body: data})
+            .then(response => response.json())
+            .then(result => {
+                console.log('Success:', result);
+    })
+})
+    if (message.content == '.end_stream'){
+        console.log('Ending stream...')s
     }
 })
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN)
